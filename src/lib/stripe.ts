5cea6d1df17632e070c23656d2f4398f3d4e9572
veraianatural <veraianatural@gmail.com>
2026-06-19 00:34:29 +0200

    Add files via upload

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,2 +1,3 @@
 import Stripe from 'stripe';
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', { apiVersion: '2024-12-18.acacia' });
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '');
